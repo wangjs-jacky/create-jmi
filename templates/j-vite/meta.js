a5c283115dabb5e38a02398f00311a5caa8d1a09
所无聊 <2409277719@qq.com>
2023-12-17 22:31:23 +0800

    feat: 更新 vite 模板

---
+++ templates/j-vite/meta.js
@@ -0,0 +1,32 @@
+const { basename } = require("path");
+const metaInfo = (ctxRef) => {
+  const { gitInfo } = ctxRef.current;
+  const { username, email } = gitInfo || {};
+
+  return {
+    "description": "生成 vite 工程",
+    "questions": [
+      {
+        "type": "text",
+        "name": "projectName",
+        "message": "请输入工程的名称",
+        "initial": basename(process.cwd())
+      },
+      {
+        "type": "text",
+        "name": "username",
+        "message": "请输入用户名称",
+        "initial": username
+      },
+      {
+        "type": "text",
+        "name": "email",
+        "message": "请输入 email 地址",
+        "initial": email
+      }
+    ],
+    "type": "app"
+  }
+}
+
+module.exports = metaInfo;
