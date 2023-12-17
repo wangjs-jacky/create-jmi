d9a811896ae9c2708c36116b82cd62d49bf4e727
所无聊 <2409277719@qq.com>
2023-12-17 22:31:56 +0800

    feat: 支持 jmi generate 指令

--- src/cli.ts
+++ src/cli.ts
@@ -7,6 +7,7 @@ export function run() {
   const cli = cac("jmi").version(version).help();
   cli
     .command("g [templateName]", "选择需要生成的模板,如果不选择则弹出所有 templates 下的文件夹模板")
+    .alias("generate")
     .allowUnknownOptions()
     .action((templateName: string, options: any = {}) => {
       const ctx = {
